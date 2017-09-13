c40e9ab079fb4835c98f663d0afcf52e5368a6d2
pnhung177 <pnhung177@acegik.net>
2017-09-13 13:46:46 +0700

    Fix @deprecated

--- src/main/java/com/devebot/opflow/OpflowHelper.java
+++ src/main/java/com/devebot/opflow/OpflowHelper.java
@@ -4,7 +4,7 @@ package com.devebot.opflow;
  *
  * @author drupalex
  * 
- * @deprecated  As of release 0.1.12, replaced by {@link #OpflowLoader}
+ * @deprecated  As of release 0.1.12, replaced by OpflowLoader
  */
 @Deprecated
 public class OpflowHelper extends OpflowLoader {}
