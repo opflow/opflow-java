af6d36bc09cb781dbc6ddbafdf40e317380a2948
pnhung177 <pnhung177@acegik.net>
2020-02-28 16:10:20 +0700

    Implement the Regex replaceAll(Function) in Java 8

--- src/main/java/com/devebot/opflow/supports/OpflowTextFormat.java
+++ src/main/java/com/devebot/opflow/supports/OpflowTextFormat.java
@@ -30,9 +30,46 @@ public class OpflowTextFormat {
                 return newVal;
             }
         };
-        return m.replaceAll(replacer);
+        StringBuffer result = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(result, replacer.apply(m));
+        }
+        m.appendTail(result);
+        return result.toString();
     }
     
+    /*
+    public static String format9(String format, final Map<String, Object> params) {
+        Matcher m = PH_PATTERN.matcher(format);
+        Function<MatchResult, String> replacer = new Function<MatchResult, String>() {
+            @Override
+            public String apply(MatchResult t) {
+                String matched = t.group(1);
+                String[] fieldPath = matched.split("\\.");
+                Object newObj = OpflowObjectTree.getObjectByPath(params, fieldPath);
+                String newVal = (newObj == null) ? "<null>" : newObj.toString();
+                newVal = newVal.replaceAll("\\$", ".");
+                return newVal;
+            }
+        };
+        return m.replaceAll(replacer);
+    }
+
+    public static String format8(String format, final Map<String, Object> params) {
+        Matcher m = PH_PATTERN.matcher(format);
+        StringBuffer result = new StringBuffer();
+        while (m.find()) {
+            String[] fieldPath = m.group(1).split("\\.");
+            Object newObj = OpflowObjectTree.getObjectByPath(params, fieldPath);
+            String newVal = (newObj == null) ? "<null>" : newObj.toString();
+            newVal = newVal.replaceAll("\\$", ".");
+            m.appendReplacement(result, newVal);
+        }
+        m.appendTail(result);
+        return result.toString();
+    }
+    */
+
     public static String formatLegacy(String format, Map<String, Object> params) {
         Matcher m = PH_PATTERN.matcher(format);
         String result = format;
