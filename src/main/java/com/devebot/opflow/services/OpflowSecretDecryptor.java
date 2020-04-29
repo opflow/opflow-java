8ad9928f4497fd9a9a4a17f2314052224d4930e7
pnhung177 <pnhung177@gmail.com>
2020-04-29 15:41:41 +0700

    Support decrypt the Ansible Vault block for the password fields

--- src/main/java/com/devebot/opflow/services/OpflowSecretDecryptor.java
+++ src/main/java/com/devebot/opflow/services/OpflowSecretDecryptor.java
@@ -1,5 +1,6 @@
 package com.devebot.opflow.services;
 
+import com.devebot.jigsaw.vault.core.VaultHandler;
 import com.devebot.opflow.OpflowConfig;
 import com.devebot.opflow.exception.OpflowBootstrapException;
 import com.devebot.opflow.supports.OpflowObjectTree;
@@ -14,11 +15,20 @@ import org.slf4j.LoggerFactory;
 public class OpflowSecretDecryptor implements OpflowConfig.Transformer {
     private final static Logger LOG = LoggerFactory.getLogger(OpflowSecretDecryptor.class);
     
+    private final VaultHandler handler = new VaultHandler();
+    
     @Override
     public Map<String, Object> transform(Map<String, Object> config) throws OpflowBootstrapException {
         return OpflowObjectTree.traverseTree(config, new OpflowObjectTree.LeafUpdater() {
             @Override
             public Object transform(String[] path, Object value) {
+                if (value instanceof String) {
+                    String valueStr = (String) value;
+                    if (handler.isVaultBlock(valueStr)) {
+                        return handler.decryptVault(valueStr);
+                    }
+                    return valueStr;
+                }
                 return value;
             }
         });
