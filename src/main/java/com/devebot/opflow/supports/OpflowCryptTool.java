bfe78ff96f1326b0f333faa381678804c5e88817
pnhung177 <pnhung177@gmail.com>
2020-04-29 17:00:01 +0700

    Add the OpflowCryptTool.decryptVault() method

--- src/main/java/com/devebot/opflow/supports/OpflowCryptTool.java
+++ src/main/java/com/devebot/opflow/supports/OpflowCryptTool.java
@@ -1,7 +1,8 @@
 package com.devebot.opflow.supports;
 
 import at.favre.lib.nostro.crypto.bcrypt.BCrypt;
-
+import com.devebot.jigsaw.vault.core.VaultHandler;
+import java.util.Properties;
 
 /**
  *
@@ -17,5 +18,31 @@ public class OpflowCryptTool {
     public static boolean checkPasswd(char[] password, char[] hash) {
         BCrypt.Result result = BCrypt.verifyer().verify(password, hash);
         return result.verified;
+    }
+    
+    public static Properties decryptVault(Properties source) {
+        if (source == null) {
+            return null;
+        }
+        for(String key: source.stringPropertyNames()) {
+            String valueStr = source.getProperty(key);
+            if (getVaultHandler().isVaultBlock(valueStr)) {
+                source.setProperty(key, getVaultHandler().decryptVault(valueStr));
+            }
+        }
+        return source;
+    }
+    
+    private static VaultHandler vaultHandler = null;
+    
+    public static VaultHandler getVaultHandler() {
+        if (vaultHandler == null) {
+            synchronized (VaultHandler.class) {
+                if (vaultHandler == null) {
+                    vaultHandler = new VaultHandler();
+                }
+            }
+        }
+        return vaultHandler;
     }
 }
